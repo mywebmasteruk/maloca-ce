9b6751482d0a46693abdc595150f73d6d0cb981d
myWebmasterUk <youare@mywebmaster.co.uk>
2022-08-16 12:20:44 +0400

    mixed content fixed
    home-fitness-hero-slider.html

---
+++ phpform/contactthanks.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta name="format-detection" content="telephone=no" />
+  <link rel="shortcut icon" href="/img/maloca.jpg" />
+  <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+  <link href="/css/ico-7-stroke.css" rel="stylesheet" />
+  <link href="/css/ico-helper.css" rel="stylesheet" />
+  <link href="/css/magnific-popup.css" rel="stylesheet" />
+  <link href="/css/animsition.min.css" rel="stylesheet" />
+  <link href="/css/animate.css" rel="stylesheet" />
+  <link href="/css/style.css" rel="stylesheet" type="text/css" />
+  <link href="/css/fullwidth.css" rel="stylesheet" type="text/css" />
+  <title>Maloca Creatividad Estratégica</title>
+</head>
+
+<body class=" animsition   ">
+	<!-- ///////////////////////////////////////// -->
+	<header class="u-header u-header--transp-light u-header--black-text">
+    <div class="container no-padd-lg">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="u-header__wrap">
+            <div class="u-header__logo float-right">
+              <a href="/index.html" class="table-cell">maloca</a>
+            </div>
+            <div class="u-header__nav-icon float-left">
+              <a href="#" class="table-cell"><i></i></a>
+            </div>
+            <div class="u-header__navigation float-left">
+              <nav>
+                <ul class="table-cell main-menu">
+                  <li class="menu-item menu-item-has-children"><a href="/index.html#filters">portafolio</a>
+                  </li>
+                  <li class="menu-item menu-item-has-children"><a href="/servicios.html">servicios</a>
+                  </li>
+                  <li class="menu-item menu-item-has-children u-header__megamenu"><a href="/index.html#contact">contacto</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ///////////////////////////////////////// -->
+  <!-- ///////////////////////////////////////// -->
+  
+
+  <div class="wpc-our-creative bg-white">
+    <div class="container no-padd-lg">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="u-section-heading u-section-heading--only-title">
+            <div class="u-section-heading__header">
+              <h1 class="u-section-heading__title u-separator u-separator--left">GRACIAS</h1>
+			  <p class="u-section-heading__title u-separator--left"></p>
+			  <p class="u-section-heading__title u-separator--left">Su mensaje ha sido enviado</p>
+			</div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ///////////////////////////////////////// -->
+  <footer class="u-footer" id="contact">
+    <div class="u-footer__top">
+      <div class="container no-padd-lg">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 marg-sm-b60">
+            <div class="u-footer__item">
+              <h3 class="u-footer__title">aún más cerca</h3>
+              <ul class="u-footer-info marg-lg-b60">
+                <li>
+                  <a href="#" class="u-footer-info__desc">(+34) 93 458 3726</a>
+                </li>
+                <li>
+                  <a href="#" class="u-footer-info__desc">infomaloca@maloca-ce.com</a>
+                </li>
+                </ul>
+              <h3 class="u-footer__title">síguenos</h3>
+              <ul class="u-footer-soc">
+                <li>
+                  <a target="_blank" href="https://www.linkedin.com/company/maloca-creatividad-estrategica" class="fa fa-linkedin"></a>
+                </li>
+                <li>
+               
+                  <a  style="display: none;" href="#" class="fa fa-facebook"></a>
+                </li>
+                <li>
+                  <a  style="display: none;" href="#" class="fa fa-twitter"></a>
+                </li>
+                <li>
+                  <a  style="display: none;" href="#" class="fa fa-google-plus"></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 marg-sm-b60">
+            <div class="u-footer__item">
+              <h3 class="u-footer__title">hablemos de tu proyecto</h3>
+              
+              
+              
+              
+              <form method="post" action="/phpform/contactengine.php" class="u-footer-form">
+                <input class="u-footer-form__input" required placeholder="tu nombre" type="text" name="Name" id="Name" />
+                <input class="u-footer-form__input" required placeholder="tu email" type="text" name="Email" id="Email" />
+                <textarea class="u-footer-form__textarea" name="Message" rows="20" cols="20" id="Message" required placeholder="cuentanos tu idea"></textarea>
+        
+                <input class="u-footer-form__btn u-btn u-btn--small u-btn--grey u-btn--hover-grey" type="submit" name="submit" value="Enviar" class="submit-button" />
+              </form>
+              
+           
+
+
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <div class="u-footer__item" style="display: none;">
+              <h3 class="u-footer__title">puedes visitarnos aquí</h3>
+              <div class="u-footer__map wpc-map" data-lat="40.7143528" data-lng="-74.0059731" data-marker="img/marker2.png" data-zoom="10" data-style="style-2" data-string="WPC string"></div>
+              <p>
+               <a href="#" class="u-footer-info__desc">calle montserrat 25B. 08810 barcelona (sant pere de ribes).</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="u-footer__bottom">
+      <a href="/index.html" class="u-footer__main-link">Copyrignt® maloca creatividad estrategica</a>
+    </div>
+  </footer>
+  <!-- ///////////////////////////////////////// -->
+  <script src="/js/jquery-3.3.1.min.js"></script>
+  <script src="/js/isotope.pkgd.min.js"></script>
+  <script src="/js/jquery.magnific-popup.min.js"></script>
+  <script src="/js/jquery.animsition.min.js"></script>
+  <script src="/js/jquery.countTo.js"></script>
+  <script src="/js/typed.min.js"></script>
+  <script src="/js/isotope.pkgd.min.js"></script>
+  <script src="/js/wow.min.js"></script>
+  <script src="/js/jquery.raty-fa.min.js"></script>
+  <script src="/js/jquery.matchHeight.min.js"></script>
+  <script src="/js/idangerous.swiper.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDLa0X2YHRndjJmw1rFB82NhRbjYRU1_lU" type="text/javascript"></script>
+  <script src="/js/scroll.js"></script>
+  <script src="/js/all.js"></script>
+</body>
+
+</html>
